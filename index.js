2f3e290708279509fb995da3bced4cbaee44f674
billdevmaster71 <billdevmaster@gmail.com>
2023-08-17 03:24:40 +0900

    fix: udpate logic

--- index.js
+++ index.js
@@ -7,36 +7,7 @@ const Web3 = require('web3');
 const axios = require('axios');
 require('dotenv').config();
 const app = express();
-const tokenAddress = "0x690c98145025dE4Ff90d4F5781f53aa35eF05544";
-const adminAddress = "0xf3BD9c66deC63b71179De31de64Af518F0B79723";
-const web3 = new Web3(new Web3.providers.HttpProvider("https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"));
-const tokenABI = [
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "recipient",
-        "type": "address"
-      },
-      {
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
-      }
-    ],
-    "name": "transfer",
-    "outputs": [
-      {
-        "internalType": "bool",
-        "name": "",
-        "type": "bool"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
-];
-const contractInstance = new web3.eth.Contract(tokenABI, tokenAddress);
+
 const secretEncryptionKey = process.env.secretKey;
 const configuration = {
   host: process.env.host,
@@ -48,138 +19,62 @@ const configuration = {
 const rate = 2; // 100 coins = 200 token
 let connection;
 
-
 handleDisconnect();
-
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use(cors({ origin: '*' }));
 // Define a route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.post('/saveGameData', async (req, res) => {
-  const data = req.body;
-  const hash = req.query.hash;
-  const input = data.address + data.coins + secretEncryptionKey;
+app.post('/saveScore', async (req, res) => {
+  
+  const {hash, address, score} = req.body;
+  const input = address + score + secretEncryptionKey;
+  console.log(input);
   const hashServer = hashString(input);
+  console.log(hashServer)
+  console.log(hash)
   let sql = "";
   if (hashServer == hash) {
     // save 
-    sql = `SELECT * FROM gameData WHERE address='${data.address}'`;
-    connection.query(sql, (err, result) => {
-      if (err) throw err;
-      if (result.length > 0) { // update
-        sql = `UPDATE gameData SET 
-          sound=${data.sound}, 
-          music=${data.music}, 
-          highscore=${data.highscore}, 
-          playcount=${data.playcount}, 
-          ads=${data.ads}, 
-          coins=${data.coins}, 
-          xp=${data.xp}, 
-          level=${data.level}, 
-          installDays=${data.installDays}, 
-          selectedTheme='${data.selectedTheme}', 
-          classicSevenBetStep=${data.classicSevenBetStep}, 
-          volume=${data.volume} 
-          WHERE address='${data.address}'`;
-      } else { // insert
-        sql = `INSERT INTO gameData (
-          address, 
-          sound, 
-          music, 
-          highscore, 
-          playcount, 
-          ads, 
-          coins, 
-          xp, 
-          level, 
-          installDays, 
-          selectedTheme, 
-          classicSevenBetStep, 
-          volume) VALUES (
-          '${data.address}', 
-          ${data.sound}, 
-          ${data.music}, 
-          ${data.highscore}, 
-          ${data.playcount}, 
-          ${data.ads}, 
-          ${data.coins}, 
-          ${data.xp}, 
-          ${data.level}, 
-          ${data.installDays}, 
-          '${data.selectedTheme}', 
-          ${data.classicSevenBetStep}, 
-          ${data.volume})`;
-      }
+      sql = `INSERT INTO scores (
+        address, 
+        score) VALUES (
+        '${address}', 
+        ${score})`;
+    try {
       connection.query(sql, (err1, result1) => {
         if (err1) throw err1;
         console.log(result1);
         res.json("success");
       });
-    });
+    } catch (e) {
+      res.json("fail");
+    }
+  } else {
+    res.json("fail");
   }
 });
 
-app.get('/getGameData', (req, res) => {
-  const address = req.query.address;
-  const sql = `SELECT * FROM gameData WHERE address='${address}'`;
-  connection.query(sql, (err, result) => {
+app.get('/getScore', async (req, res) => {
+  const { start, limit } = req.query;
+  const sql = `SELECT address, score from (SELECT address, MAX(score) as score FROM scores GROUP BY address) as t1 ORDER BY t1.score DESC LIMIT ${start}, ${limit}`;
+  connection.query(sql, async (err, result) => {
     if (err) throw err;
     if (result.length > 0) {
-      let retData = {...result[0], rate};
-      res.json(retData);
+      const tmpArr = []
+      for (let i = 0; i < result.length; i++) {
+        const sql1 = `SELECT address, createdAt, score FROM scores WHERE address='${result[i].address}' AND score=${result[i].score} ORDER BY createdAt LIMIT 1`;
+        const result1 = await executeQuery(sql1);
+        tmpArr.push({ address: result1[0].address, score: result1[0].score, createdAt: result1[0].createdAt });
+      }
+      res.json(tmpArr);
     } else {
-      res.json({
-        rate,
-        sound: "1",
-        music: "1",
-        highscore: 0,
-        playcount: 0,
-        ads: "1",
-        coins: 0,
-        xp: 0,
-        level: 0,
-        installDays: 0,
-        selectedTheme: "",
-        classicSevenBetStep: 0,
-        volume: 0
-      });
+      res.json([]);
     }
   })
 });
-
-app.post('/sellCoins', async (req, res) => {
-  const amount = req.query.amount * rate;
-  const address = req.query.address;
-  const encodedFunction = contractInstance.methods.transfer(address, Web3.utils.toWei(amount.toString(), 'ether')).encodeABI();
-  let trxHash = "";
-  const txObject = {
-    to: tokenAddress,
-    data: encodedFunction,
-    gas: 200000, // Set an appropriate gas limit
-    gasPrice: web3.utils.toWei('10', 'gwei'), // Convert gas price to Wei
-  };
-  try {
-    const account = web3.eth.accounts.privateKeyToAccount(process.env.mnemonic);
-    const signedTx = await account.signTransaction(txObject);
-    web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-    .on('transactionHash', (hash) => {
-      trxHash = hash;
-    })
-    .on('receipt', (receipt) => {
-      return res.send(`success:${trxHash}`);
-    })
-    .on('error', (error) => {
-      console.log(error)
-      return res.send("error:")
-    });
-
-  } catch (e) {
-    console.log(e)    
-  }
-})
 
 // Start the server
 app.listen(process.env.PORT, () => {
@@ -213,80 +108,21 @@ function handleDisconnect() {
   });
 }
 
-async function detectTransactions() {
-  
-  let lastBlock = await web3.eth.getBlockNumber();
-  console.log(lastBlock);
-  while(true) {
-    try {
-      const resp = await axios.get(`https://api-goerli.etherscan.io/api?module=logs&action=getLogs&fromBlock=${lastBlock + 1}&address=${tokenAddress}&apikey=TEJSMRD3P2TQYCCIT5XC7C3K5U11CE4QSG`);
-
-      if (resp.data.result?.length > 0) {
-        for (let i = 0; i < resp.data.result.length; i++) {
-          const trxHash = resp.data.result[i].transactionHash;
-          const transaction = await web3.eth.getTransaction(trxHash);
-          const input = transaction.input;
-
-          if (input.slice(0, 10) === '0xa9059cbb' && transaction.to.toLowerCase() == tokenAddress.toLowerCase()) { // transfer function
-            const recipient = '0x' + input.slice(34, 74);
-            const from = transaction.from;
-            const amount = web3.utils.toBN('0x' + input.slice(74));
-            const addedCoinAmount = web3.utils.fromWei(amount, "ether") * (1 / rate);
-            
-            let sql = "";
-            let address = "";
-            let type = "";
-            if (recipient.toLowerCase() == adminAddress.toLowerCase()) { // buy coins
-              address = from.toLowerCase();
-              type = "buy";
-              sql = `SELECT * FROM gameData WHERE address='${from.toLowerCase()}'`;
-              connection.query(sql, (err1, result1) => {
-                if (err1) throw err1;
-                if (result1.length > 0) {
-                  sql = `UPDATE gameData SET coins=coins + ${addedCoinAmount} WHERE address='${from.toLowerCase()}'`;
-                } else {
-                  sql = `INSERT INTO gameData (address, coins) VALUES ('${from.toLowerCase()}', ${addedCoinAmount})`;
-                }
-                connection.query(sql, (err, result) => {
-                  if (err) throw err;
-                  insertLog(address, type, amount, trxHash, addedCoinAmount)
-                })
-              })
-            } else { // sell coins
-              address = recipient.toLowerCase();
-              type = "sell";
-              let sql = `UPDATE gameData SET coins=coins - ${addedCoinAmount} WHERE address='${recipient.toLowerCase()}'`;
-              
-              connection.query(sql, (err, result) => {
-                if (err) throw err;
-                insertLog(address, type, amount, trxHash, addedCoinAmount)
-              })
-            }
-          }
-          const blockNumber = parseInt(resp.data.result[i].blockNumber, 16);
-          lastBlock = blockNumber;
-        }
-      } else {
-        await delay(1000);
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
-}
-
 const delay = ms => {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 };
 
-const insertLog = function (address, transaction_type, token_amount, transaction_hash, coins_amount) {
-  let sql = `INSERT INTO logs (address, transaction_type, token_amount, transaction_hash, coins_amount, token_num) 
-              VALUES ('${address}', '${transaction_type}', ${token_amount}, '${transaction_hash}', ${coins_amount}, ${web3.utils.fromWei(token_amount, "ether")})`;
-  connection.query(sql, (err, result) => {
-    if (err) throw err;
-  })
-}
-
-detectTransactions();
+// Execute the query using await
+const executeQuery = (sql) => {
+  return new Promise((resolve, reject) => {
+    connection.query(sql, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
